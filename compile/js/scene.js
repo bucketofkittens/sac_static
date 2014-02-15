9d01fc15fc18a2254e98f75feae585b3de6f6415
Novikov Andrey <anovikov@at-consulting.ru>
2014-02-15 10:17:29 +0400

    Some scene widgets flesh

--- compile/js/scene.js
+++ compile/js/scene.js
@@ -41,6 +41,7 @@ var SceneInfoWidget = function(app) {
     }
 
     this.show = function() {
+        this.showInfo(false);
         this.elements["MAIN"].removeClass('hidden');
     }
 
@@ -51,6 +52,64 @@ var SceneInfoWidget = function(app) {
     this.setContent = function (content) {
         this.elements["SCENEINFO"].html(content);
     }
+
+    this.showInfo = function (hide) {
+        that = this;
+        if (hide) that.elements["SCENEINFO"].addClass('hidden');
+        $('#sceneinfo-main', this.elements["MAIN"]).siblings().removeClass('current');
+        $('#sceneinfo-main', this.elements["MAIN"]).addClass('current');
+        setTimeout(function() {
+            $.get('/static/compile/scene/info.html', {}, function (data, status, jqxhr) {
+                that.elements["SCENEINFO"].html(data);
+                that.elements["SCENEINFO"].removeClass('hidden');
+                // Page stuff
+                var menu_items = $("#scene-info-selector li");
+                menu_items.on('click', function () {
+                    menu_items.removeClass('current');
+                    $(this).addClass('current');
+                    var target = $('#'+$(this).attr('data-target'));
+                    target.removeClass('hidden-left hidden-right hidden');
+                    target.prevAll('section').addClass('hidden hidden-left').removeClass('hidden-right');
+                    target.nextAll('section').addClass('hidden hidden-right').removeClass('hidden-left');
+                });
+            });
+        }, (hide ? 400 : 0));
+    }
+
+    this.showMap = function () {
+        that = this;
+        that.elements["SCENEINFO"].addClass('hidden');
+        $('#sceneinfo-map', this.elements["MAIN"]).siblings().removeClass('current');
+        $('#sceneinfo-map', this.elements["MAIN"]).addClass('current');
+        setTimeout(function() {
+            $.get('/static/compile/scene/map.html', {}, function (data, status, jqxhr) {
+                that.elements["SCENEINFO"].html(data);
+                that.elements["SCENEINFO"].removeClass('hidden');
+                // Map stuff
+                // TODO: Map scripts
+            });
+        }, 400);
+    }
+
+    this.showForces = function () {
+        that = this;
+        that.elements["SCENEINFO"].addClass('hidden');
+        $('#sceneinfo-forces', this.elements["MAIN"]).siblings().removeClass('current');
+        $('#sceneinfo-forces', this.elements["MAIN"]).addClass('current');
+        setTimeout(function() {
+            $.get('/static/compile/scene/forces.html', {}, function (data, status, jqxhr) {
+                that.elements["SCENEINFO"].html(data);
+                that.elements["SCENEINFO"].removeClass('hidden');
+                // Forces stuff
+                // TODO: Forces scripts
+            });
+        }, 400);
+    }
+
+    // Bind events to main menu
+    $('#sceneinfo-main   a', this.elements["MAIN"]).on('click', $.proxy(this.showInfo, this));
+    $('#sceneinfo-map    a', this.elements["MAIN"]).on('click', $.proxy(this.showMap, this));
+    $('#sceneinfo-forces a', this.elements["MAIN"]).on('click', $.proxy(this.showForces, this));
 
 }
 
