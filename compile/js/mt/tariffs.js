7d8ce83dfbebacb76e23aa5e0e9ea492526ab608
lyykfi <lyykfi@gmail.com>
2014-04-09 17:34:31 +0400

    fake

--- compile/js/mt/tariffs.js
+++ compile/js/mt/tariffs.js
@@ -453,8 +453,8 @@ TariffCamersMainWidget.onCamera_ = function(e) {
     var enterIp = $(e.target).val();
     var index = $("#edit_camera_index_old").val();
     var camera = this.getCameraByIndex_(index);
-    
-    var html = '<embed allowfullscreen="false" controls="false" toolbar="false" controls="false" toolbar="false" type="application/x-fb-vlc" pluginspage="http://www.videolan.org" version="VideoLAN.VLCPlugin.2"  width="400px"  height="300px" id="vlc" loop="yes" autoplay="yes" target="'+ip+'"></embed>';
+
+    var html = '<embed allowfullscreen="false" controls="false" toolbar="false" controls="false" toolbar="false" type="application/x-mplayer2" pluginspage="http://www.videolan.org" version="VideoLAN.VLCPlugin.2"  width="400px"  height="300px" id="vlc" loop="yes" autoplay="yes" src="'+ip+'"></embed>';
     $("#in_cam_add_camera").html(html);
     $(".camera-add-view").hide();
     
@@ -470,7 +470,7 @@ TariffCamersMainWidget.onCameraEdit_ = function(e) {
 
     ip = showFakeCamera(camera, enterIp);
 
-    var html = '<embed allowfullscreen="false" controls="false" toolbar="false" controls="false" toolbar="false" type="application/x-fb-vlc" pluginspage="http://www.videolan.org" version="VideoLAN.VLCPlugin.2"  width="400px"  height="300px" id="vlc" loop="yes" autoplay="yes" target="'+ip+'"></embed>';
+    var html = '<embed allowfullscreen="false" controls="false" toolbar="false" controls="false" toolbar="false" type="application/x-vlc-plugin" pluginspage="http://www.videolan.org" version="VideoLAN.VLCPlugin.2"  width="400px"  height="300px" id="vlc" loop="yes" autoplay="yes" target="'+ip+'"></embed>';
     $("#in_cam_edit_camera").html(html);
     $(".camera-edit-view").hide();
     //$(".camera-add-view").attr("src", "rtsp://"+$(e.target).val()+"/video.pro1");
@@ -807,7 +807,7 @@ function showFakeCamera(camera, enterIp) {
 }
 
 function getVideoTag(ip) {
-    var html = '<embed controls="false" class="ramp-frame  ramk-edit-view" id="#in_cam_edit_ramkc" toolbar="false" type="application/x-fb-vlc" pluginspage="http://www.videolan.org" version="VideoLAN.VLCPlugin.2"  width="400px"  height="300px" id="vlc" loop="yes" autoplay="yes" target="'+ip+'" allowfullscreen="false" controls="false" toolbar="false"></embed>';
+    var html = '<embed controls="false" class="ramp-frame  ramk-edit-view" id="#in_cam_edit_ramkc" toolbar="false" type="application/x-vlc-plugin" pluginspage="http://www.videolan.org" version="VideoLAN.VLCPlugin.2"  width="400px"  height="300px" id="vlc" loop="yes" autoplay="yes" target="'+ip+'" allowfullscreen="false" controls="false" toolbar="false"></embed>';
     return html;
 }
 
